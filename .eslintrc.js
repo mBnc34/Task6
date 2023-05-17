5ac013198bedd2419d0f8caa18e8c463cb675afd
Moussa Benchikh <104487495+mBnc34@users.noreply.github.com>
2023-05-17 13:12:51 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
         ],
         "quotes": [
             "error",
-            "double"
+            "single"
         ],
         "no-unused-vars": "off",
         "semi": [
